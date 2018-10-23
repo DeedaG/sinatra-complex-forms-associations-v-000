ab269ace3a412bdc951fc8901dbec056ed700eb9
Christia T <christiatidwell@bellsouth.net>
2018-10-23 19:21:52 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,7 +33,7 @@ class PetsController < ApplicationController
     end
 
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.update(name: params["owner"])
+      @pet.owner = Owner.update(name: params["owner"]["name"], owner_id: params["owner"]["owner_id"])
     end
     redirect "pets/#{@pet.id}"
   end
